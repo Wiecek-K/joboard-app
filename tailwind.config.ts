89e7892fb2cf8d7f2be51ebb8d48613f37ae2560
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2024-06-11 19:15:24 +0200

    defined tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,23 @@ const config: Config = {
       './src/app/**/*.{js,ts,jsx,tsx,mdx}',
    ],
    theme: {
+      colors: {
+         transparent: 'transparent',
+         current: 'currentColor',
+         white: '#ffffff',
+         accent: {
+            DEFAULT: '#9564D4',
+            strong: '#552882',
+         },
+         gray: {
+            outline: '#54545414',
+            lightest: '#F8F8F8',
+            light: '#DADADA',
+            medium: '#BCBCBC',
+            dark: '#545454',
+            darkest: '#212121',
+         },
+      },
       extend: {
          backgroundImage: {
             'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -23,6 +40,9 @@ const config: Config = {
             semibold20: ['20px', { lineHeight: '26px', fontWeight: '600', letterSpacing: '-2%' }],
             semibold24: ['24px', { lineHeight: '100%', fontWeight: '600' }],
             semibold28: ['28px', { lineHeight: '100%', fontWeight: '600' }],
+         },
+         boxShadow: {
+            checkbox: '0px 0px 4px 0px hsla(266, 57%, 61%, 0.4)',
          },
       },
    },
