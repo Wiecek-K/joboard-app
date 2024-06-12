32bd5a888081f20acc332de2e53141667717bd27
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2024-06-12 14:31:06 +0200

    convert icons iton tsx components

---
+++ src/assets/icons/ChevronDown.tsx
@@ -0,0 +1,10 @@
+export const ChevronDown = () => (
+   <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+         d="M11 14L15 17.5L19 14"
+         stroke="#545454"
+         stroke-linecap="round"
+         stroke-linejoin="round"
+      />
+   </svg>
+);
