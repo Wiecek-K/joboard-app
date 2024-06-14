6d1bd74206f6160f3b23cac19944514875e2e0fd
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2024-06-15 01:15:14 +0200

    added bold text in suggestions , fix ux issues

--- src/ui/components/OfferList.tsx
+++ src/ui/components/OfferList.tsx
@@ -4,15 +4,18 @@ import { JobOfferI } from '@/lib/types';
 import { OfferCard } from './OfferCard';
 import useSWR from 'swr';
 import { fetchAllOffers } from '@/lib/data';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { Search } from '@/assets/icons/Search';
 interface OfferListProps {
    offersArray: JobOfferI[];
 }
 
 export const OfferList = ({ offersArray }: OfferListProps) => {
-   const { data, error } = useSWR<JobOfferI[]>('jobOffers', fetchAllOffers);
+   const { data, error, isLoading } = useSWR<JobOfferI[]>('jobOffers', fetchAllOffers);
    const [wantedTitle, setWantedTitle] = useState('');
+
+   const titleSerchBarRef = useRef<HTMLDivElement>(null);
 
    const searchParams = useSearchParams();
    const pathname = usePathname();
@@ -29,18 +32,25 @@ export const OfferList = ({ offersArray }: OfferListProps) => {
       }
       replace(`${pathname}?${params.toString()}`);
       setWantedTitle(term);
+      titleSerchBarRef.current?.blur();
    };
 
    if (error) return <div>Wystąpił błąd</div>;
-   if (!data) return <div>Ładowanie...</div>;
+   if (isLoading) return <div>Ładowanie...</div>;
+   if (!data) throw new Error();
 
    const filteredData = data.filter((offer) =>
       offer.title.toLocaleLowerCase().includes(titlePram.toLowerCase()),
    );
+
    return (
       <div className="w-full bg-gray-lightest p-[16px] md:p-[40px]">
          <div className="mb-[24px] flex w-full gap-[12px]">
-            <div className="relative w-full">
+            <div
+               tabIndex={0}
+               className="focus-within:signal relative w-full"
+               ref={titleSerchBarRef}
+            >
                <input
                   onChange={(e) => setWantedTitle(e.target.value)}
                   value={wantedTitle}
@@ -50,23 +60,43 @@ export const OfferList = ({ offersArray }: OfferListProps) => {
                      if (e.key === 'Enter') handleTitleSearch(wantedTitle);
                   }}
                ></input>
-               <div className="absolute bottom-[] z-10 flex w-full flex-col border border-b-0 border-gray-light bg-white text-regular14 shadow-checkbox">
+               <div className="bottom signal:flex absolute z-10 mt-[2px]  w-full flex-col border border-b-0 border-gray-light bg-white text-regular14 shadow-checkbox ">
                   {wantedTitle &&
-                     wantedTitle !== titlePram &&
                      data
                         .filter((offer) =>
                            offer.title.toLocaleLowerCase().includes(wantedTitle.toLowerCase()),
                         )
-                        .map((offer) => (
-                           <div
-                              key={wantedTitle + offer._id}
-                              className="flex justify-between border-b border-gray-light px-[15px] py-[4px]"
-                              onClick={() => handleTitleSearch(offer.title)}
-                           >
-                              <p>{offer.title}</p>
-                              <p className="text-right text-regular12">{offer.companyName}</p>
-                           </div>
-                        ))}
+                        .map((offer) => {
+                           const titleParts = offer.title.split(
+                              new RegExp(`(${wantedTitle})`, 'gi'),
+                           );
+                           return (
+                              <div
+                                 key={wantedTitle + offer._id}
+                                 className="flex min-h-[30px] cursor-pointer items-center justify-between border-b border-gray-light px-[15px] py-[4px]"
+                                 onClick={() => handleTitleSearch(offer.title)}
+                              >
+                                 <p className="max-w-[75%]t">
+                                    {titleParts.map((part, index) =>
+                                       part.toLowerCase() === wantedTitle.toLowerCase() ? (
+                                          <span key={index} className="font-bold">
+                                             {part}
+                                          </span>
+                                       ) : (
+                                          <span key={index}>{part}</span>
+                                       ),
+                                    )}
+                                 </p>
+                                 <p className="text-right text-regular12">{offer.companyName}</p>
+                              </div>
+                           );
+                        })}
+               </div>
+               <div
+                  className="absolute right-[10px] top-[50%] -translate-y-1/2 cursor-pointer"
+                  onClick={() => handleTitleSearch(wantedTitle)}
+               >
+                  <Search />
                </div>
             </div>
             <div className="relative w-full"></div>
