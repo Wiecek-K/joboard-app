e5e5c108e6308ecade5df2262edc302a15c77b34
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2024-07-16 03:01:58 +0200

    adjust to mobile devices

--- src/ui/components/OfferDetailsModal/OfferDetailsModal.tsx
+++ src/ui/components/OfferDetailsModal/OfferDetailsModal.tsx
@@ -9,8 +9,7 @@ import grid from './OfferDetailsModal.module.css';
 import clsx from 'clsx';
 import { calculateDaysElapsed, formatSalary } from '@/lib/utils';
 import { Close } from '@/assets/icons/Close';
-import { Search } from '@/assets/icons/Search';
-import { CheckboxMark } from '@/assets/icons/CheckboxMark';
+import exampleLogo from '@/assets/exampleCompanyLogo.jpg';
 
 interface OfferDetailsModalProps {
    setShowModal: (newState: boolean) => void;
@@ -47,20 +46,30 @@ export const OfferDetailsModal = ({ setShowModal, offerId }: OfferDetailsModalPr
    if (modalRoot) {
       return ReactDOM.createPortal(
          <div
-            className="fixed left-0 top-0 flex h-screen w-screen justify-center bg-[#00000033] p-[40px]"
-            onClick={closeModal}
+            className="absolute left-0 top-0 flex w-full justify-center bg-white md:fixed md:bg-[#00000033] md:p-[40px] min-h-screen"
+            onClick={(event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+               if (event.target !== event.currentTarget) {
+                  return;
+               }
+               closeModal();
+            }}
          >
-            <div className="relative w-full max-w-[1094px] rounded-[5px] border border-gray-light bg-white pb-[48px] pl-[60px] pr-[74px] pt-[65px]">
-               <div className={clsx(grid.container, '')}>
-                  <div className={clsx(grid.title, 'mb-[6px] flex items-center')}>
+            <div className="relative w-full max-w-[1094px] border-gray-light bg-white px-[16px] py-[30px] md:max-h-[800px] md:rounded-[5px] md:border md:pb-[48px] md:pl-[60px] md:pr-[74px] md:pt-[65px]">
+               <div className={clsx(grid.container, 'flex flex-col gap-[16px] md:grid')}>
+                  <div
+                     className={clsx(
+                        grid.title,
+                        'mb-[6px] flex flex-col md:flex-row md:items-center',
+                     )}
+                  >
                      <Image
-                        src={data?.image || ''}
+                        src={data?.image || exampleLogo}
                         width={80}
                         height={80}
-                        className="mr-[12px]"
+                        className="mb-[28px] mr-[12px] h-[40px] w-[40px] md:mb-0 md:h-[80px] md:w-[80px]"
                         alt={`Logo of ${data?.companyName}`}
                      />
-                     <div className="flex flex-col items-start gap-[16px]">
+                     <div className="flex flex-col items-start gap-[12px] md:gap-[16px]">
                         <h3 className="text-semibold28">{data?.title}</h3>
                         <div className="text-regular14 text-gray-dark">
                            {data?.technologies.map((technology, index, array) =>
@@ -69,57 +78,43 @@ export const OfferDetailsModal = ({ setShowModal, offerId }: OfferDetailsModalPr
                         </div>
                      </div>
                   </div>
-                  <div
-                     className={clsx(
-                        grid.description,
-                        'bg-gray-lightest px-[40px] pt-[40px] text-regular14',
-                     )}
-                  >
-                     <p className="h-full overflow-auto whitespace-normal">{data?.description}</p>
-                  </div>
-                  <div
-                     className={clsx(grid.button, 'flex items-center bg-gray-lightest px-[30px]')}
-                  >
-                     <a className="w-full" href={data?.offerUrl} target="_blank">
-                        <Button className="w-full justify-center">Visit offer ➔</Button>
-                     </a>
-                  </div>
-                  <div className={clsx(grid.details, 'overflow-y-auto')}>
-                     <div className="flex flex-col gap-[20px] bg-gray-lightest p-[30px]">
-                        <div className="flex items-center justify-between border-b border-gray-light pb-[20px]">
+
+                  <div className={clsx(grid.details, 'md:overflow-y-auto')}>
+                     <div className="flex flex-col gap-[12px] bg-gray-lightest px-[20px] py-[26px] md:gap-[20px] md:p-[30px]">
+                        <div className="flex items-center justify-between border-b border-gray-light pb-[12px] md:pb-[20px]">
                            <p className="text-semibold12 text-gray-darkest">Added</p>
                            <p className="text-right text-regular14 text-gray-dark">
                               {calculateDaysElapsed(data?.createdAt || '0')} Days ago
                            </p>
                         </div>
-                        <div className="flex items-center justify-between border-b border-gray-light pb-[20px]">
+                        <div className="flex items-center justify-between border-b border-gray-light pb-[12px] md:pb-[20px]">
                            <p className="text-semibold12 text-gray-darkest">Company</p>
                            <p className="text-right text-regular14 text-gray-dark">
                               {data?.companyName}
                            </p>
                         </div>
 
-                        <div className="flex items-center justify-between border-b border-gray-light pb-[20px]">
+                        <div className="flex items-center justify-between border-b border-gray-light pb-[12px] md:pb-[20px]">
                            <p className="text-semibold12 text-gray-darkest">Seniority</p>
                            <p className="text-right text-regular14 text-gray-dark">
                               {data?.seniority}
                            </p>
                         </div>
-                        <div className="flex items-center justify-between border-b border-gray-light pb-[20px]">
+                        <div className="flex items-center justify-between border-b border-gray-light pb-[12px] md:pb-[20px]">
                            <p className="text-semibold12 text-gray-darkest">Location</p>
                            <p className="text-right text-regular14 text-gray-dark">
                               {data?.city},<br />
                               {data?.country}
                            </p>
                         </div>
-                        <div className="flex items-center justify-between border-b border-gray-light pb-[20px]">
+                        <div className="flex items-center justify-between border-b border-gray-light pb-[12px] md:pb-[20px]">
                            <p className="text-semibold12 text-gray-darkest">Job type</p>
                            <p className="text-right text-regular14 text-gray-dark">
                               {data?.jobType},<br />
                               {data?.workLocation}
                            </p>
                         </div>
-                        <div className="flex items-center justify-between border-b border-gray-light pb-[20px]">
+                        <div className="flex items-center justify-between border-b border-gray-light pb-[12px] md:pb-[20px]">
                            <p className="text-semibold12 text-gray-darkest">Contract</p>
                            <p className="text-right text-regular14 text-gray-dark">
                               B2B,
@@ -136,9 +131,29 @@ export const OfferDetailsModal = ({ setShowModal, offerId }: OfferDetailsModalPr
                         </div>
                      </div>
                   </div>
+
+                  <div
+                     className={clsx(
+                        grid.button,
+                        'flex items-center bg-gray-lightest px-[30px] py-[16px] md:py-0',
+                     )}
+                  >
+                     <a className="w-full" href={data?.offerUrl} target="_blank">
+                        <Button className="w-full justify-center">Visit offer ➔</Button>
+                     </a>
+                  </div>
+                  <div
+                     className={clsx(
+                        grid.description,
+                        'bg-gray-lightest px-[40px] pt-[40px] text-regular14 md:overflow-auto',
+                     )}
+                  >
+                     <p className="h-full whitespace-normal">{data?.description}</p>
+                  </div>
                </div>
+
                <button
-                  className="absolute right-[20px] top-[20px] flex h-[40px] w-[40px] items-center justify-center bg-transparent p-0"
+                  className="absolute right-[15px] top-[30px] flex h-[40px] w-[40px] items-center justify-center bg-transparent p-0 md:right-[20px] md:top-[20px]"
                   onClick={closeModal}
                >
                   <div className="relative h-full w-full">
